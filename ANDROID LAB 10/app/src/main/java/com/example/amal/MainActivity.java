552395edd5187a305cd42795fe55c1dbfe6766cf
Muhammed Anas vemmully <anasvemmully@gmail.com>
2022-02-19 16:11:08 +0530

    Fetch data from an EditText and display it in a TextView

---
+++ ANDROID LAB 10/app/src/main/java/com/example/amal/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.amal;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    EditText editText;
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.fetch);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+    }
+
+
+
+
+
+}
