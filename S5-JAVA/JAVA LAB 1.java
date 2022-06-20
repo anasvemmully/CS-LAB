8c5242907ab9d92f8fb6917dd54d0143b17bcb58
anasvemmully <anasvemmully@gmail.com>
2022-06-20 18:48:32 +0530

    Armstrong number

---
+++ S5-JAVA/JAVA LAB 1.java
@@ -0,0 +1,30 @@
+// Write a java program to display Fibinocci series upto a limit
+
+import java.util.*;
+class pg
+{
+    public static void main(String args[])
+    {
+        Scanner in =new Scanner(System.in);
+        int l,h,i,rem,sum,num;
+        System.out.println("ENTER THE LOWER LIMIT AND UPPER LIMIT:");
+        l=in.nextInt();
+        h=in.nextInt();
+
+        System.out.println("ARMSTRONG NUMBERS:");
+        for(i=l;i<=h;i++)
+        {
+            num=i;
+            sum=0;
+            while(num!=0)
+            {
+                rem=num%10;
+                int b = Integer.valueOf(String.valueOf(i).length());
+                sum=(int) (sum+Math.pow(rem,b));
+                num=num/10;
+            }
+            if(sum==i)
+            System.out.println(i);
+        }
+    }
+}
